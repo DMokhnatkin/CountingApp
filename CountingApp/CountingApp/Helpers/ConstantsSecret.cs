e68008b31a385e937745c1fef04de86504eeb921
DMokhnatkin <dmitriy.mokhnatkin@gmail.com>
2017-10-15 22:46:08 +0300

    Removed file with client secrets from git.

--- CountingApp/CountingApp/Helpers/ConstantsSecret.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CountingApp.Helpers
-{
-    public static class ConstantsSecret
-    {
-        public static string AndroidClientId = "<client_id>";
-        public static string AndroidClientSecret = "<client_secret>";
-    }
-}
